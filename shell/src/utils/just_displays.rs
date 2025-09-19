5796ad0e7306a4e8846ff356fac6badfbffab8ce
meljanat <janatimouad7@gmail.com>
2025-09-19 16:34:10 +0100

    echo fixed

--- shell/src/utils/just_displays.rs
+++ shell/src/utils/just_displays.rs
@@ -1,5 +1,80 @@
+use std::io::{self, Write};
+
 pub fn echo(input: &[&str]) {
-    println!("{}", input.join(" "));
+    let initial = input.join(" ");
+
+    if normal(&initial, ' ') {
+        println!("{}", skip_quotes(&initial));
+    } else {
+        let mut result = initial.clone();
+        let mut quote_char = which_quotes(&initial);
+
+        loop {
+            print!("> ");
+            io::stdout().flush().unwrap();
+
+            let mut extra = String::new();
+            if io::stdin().read_line(&mut extra).unwrap() == 0 {
+                break;
+            }
+
+            result.push_str("\n");
+            result.push_str(extra.trim_end());
+
+            if normal(&extra, quote_char) {
+                break;
+            }
+            quote_char = which_quotes(&result);
+        }
+        println!("{}", skip_quotes(&result));
+    }
+}
+
+fn normal(input: &str, qt: char) -> bool {
+    let mut quote: char = qt;
+    for c in input.chars() {
+        if c == '"' || c == '\'' {
+            if quote == ' ' {
+                quote = c;
+            } else if quote == c {
+                quote = ' ';
+            }
+        }
+    }
+    quote == ' '
+}
+
+fn skip_quotes(input: &str) -> String {
+    let mut result = String::new();
+    let mut quote: char = ' ';
+    for c in input.chars() {
+        if c == '"' || c == '\'' {
+            if quote == ' ' {
+                quote = c;
+            } else if quote == c {
+                quote = ' ';
+            } else {
+                result.push(c);
+            }
+        } else {
+            result.push(c);
+        }
+    }
+    result
+}
+
+fn which_quotes(input: &str) -> char {
+    let mut quote: char = ' ';
+    for c in input.chars() {
+        if c == '"' || c == '\'' {
+            if quote == ' ' {
+                quote = c;
+            } else if quote == c {
+                quote = ' ';
+            }
+        }
+    }
+    quote
 }
 
 pub fn pwd() {
